e41ced4cb2d3d465a5a7d355487207d4a4abea71
kamikadze3113 <68239296+kamikadze3113@users.noreply.github.com>
2020-08-09 23:39:15 +0300

    Upload

---
+++ clockAnim.py
@@ -0,0 +1,16 @@
+#by kamikadze3113
+
+from telethon import events
+import asyncio
+from collections import deque
+
+
+@borg.on(events.NewMessage(pattern=r"\.cm", outgoing=True))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("🕙🕘🕗🕖🕕🕔🕓🕒🕑🕐🕛"))
+	for _ in range(96):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
